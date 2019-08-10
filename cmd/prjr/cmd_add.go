e30a75660f7a2ac32a4c99e0fdb5ff57ec0200ff
Clay Dunston <claydunston@gmail.com>
2019-08-09 21:32:09 -0500

    Prompt before adding project.

--- cmd/prjr/cmd_add.go
+++ cmd/prjr/cmd_add.go
@@ -3,18 +3,29 @@ package main
 import (
 	"fmt"
 
+	"github.com/AlecAivazis/survey"
 	"github.com/tcd/prjr/internal/prjr"
 )
 
 func addCmd(pjs prjr.Projects) {
-	pj, err := prjr.NewProjectHere()
-	if err != nil {
-		fmt.Println(err)
+	shouldAdd := false
+	prompt := &survey.Confirm{
+		Message: "Add the current directory as a project?",
 	}
+	survey.AskOne(prompt, &shouldAdd)
 
-	pjs.Add(pj)
-	err = pjs.Save()
-	if err != nil {
-		fmt.Println(err)
+	if shouldAdd {
+		pj, err := prjr.NewProjectHere()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		pjs.Add(pj)
+		err = pjs.Save()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("Added new project %q\n", pj.Root)
+		}
 	}
 }
