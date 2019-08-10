c1c840dcbd0ad2d02781f4621ddd0c7c2754ba35
Clay Dunston <claydunston@gmail.com>
2019-08-09 20:27:56 -0500

    Update Projects type
    Use a struct with Project slice as a field instead of a type alias,
    it's easier to remove a project this way.

--- internal/prjr/projects.go
+++ internal/prjr/projects.go
@@ -1,16 +1,29 @@
 package prjr
 
 // Projects is a slice of Project with convenience methods.
-type Projects []Project
+type Projects struct {
+	P []Project
+}
 
 // Add a Project to Projects.
 func (pjs *Projects) Add(p ...Project) {
 	for _, project := range p {
-		*pjs = append(*pjs, project)
+		pjs.P = append(pjs.P, project)
 	}
 }
 
 // Save writes Projecs to a user's prjr.json file.
 func (pjs Projects) Save() error {
-	return writeProjectsToFile(getProjectsFilePath(), pjs)
+	return writeProjectsToFile(getProjectsFilePath(), pjs.P)
+}
+
+// RemoveByRoot removes a Project identified by its Root.
+func (pjs *Projects) RemoveByRoot(root string) {
+	var newPjs Projects
+	for _, project := range pjs.P {
+		if project.Root != root {
+			newPjs.Add(project)
+		}
+	}
+	pjs.P = newPjs.P
 }
