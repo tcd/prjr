f9cf61662751e6816cc4baf80bb313ccbe9c66d0
Clay Dunston <claydunston@gmail.com>
2019-08-14 10:54:48 -0500

    Update contrib with working scripts :D

--- contrib/prjr-fzf.sh
+++ contrib/prjr-fzf.sh
@@ -1,24 +1,6 @@
-#!/usr/bin/env sh
+pjcd() {
+  local destination
 
-# Check if one string contains another.
-stringContain() {
-  [ -z "${2##*$1*}" ] && [ -z "$1" -o -n "$2" ];
+  destination=$(prjr list | fzf -0 -1 | awk '{print $2}')
+  cd "$destination"
 }
-
-# This wrapper is necessary because we can't change the shell's working
-# directory from inside a program.
-main() {
-  if [ "$1" = "jump" ]; then
-    local cmd_output=$(prjr "$@")
-    if stringContain 'jumpto:' "$cmd_output"; then
-      local dest=$(echo "${cmd_output}" | sed 's/.*: \(.*\)$/\1/' | sed -e 's/jumpto://g')
-      cd "${dest}" || return
-    else
-      echo "$cmd_output" && return
-    fi
-  else
-    prjr "$@" && return
-  fi
-}
-
-main "$@"
