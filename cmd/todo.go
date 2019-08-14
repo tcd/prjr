cbb6e2e23c9602ab79af61384eebe13cae7e4bf4
Clay Dunston <claydunston@gmail.com>
2019-08-14 18:49:35 -0500

    Add root flag for todo subcommand

--- cmd/todo.go
+++ cmd/todo.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
@@ -14,20 +15,26 @@ var todoCmd = &cobra.Command{
 	Use:   "todo",
 	Short: "List TODO comments in a project",
 	Run: func(cmd *cobra.Command, args []string) {
-		projects, err := prjr.GetLocalProjects()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+		root, _ := cmd.Flags().GetString("root")
+		if root != "" {
+			todoFuncFlag(root)
 		}
-		todoFunc(projects)
+		todoFuncPrompt()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(todoCmd)
+	todoCmd.Flags().String("root", "", "Path to the root of the Project to search for TODOs")
 }
 
-func todoFunc(pjs prjr.Projects) {
+func todoFuncPrompt() {
+	pjs, err := prjr.GetLocalProjects()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	root := ""
 	prompt := &survey.Select{
 		Message: "Which project would you like to check on?",
@@ -40,6 +47,26 @@ func todoFunc(pjs prjr.Projects) {
 			todos := pj.TODOs()
 			fmt.Println(cli.TabTodos(todos))
 		}
+	}
+	os.Exit(0)
+}
+
+func todoFuncFlag(root string) {
+	pjs, err := prjr.GetLocalProjects()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if !filepath.IsAbs(root) {
+		fmt.Fprintln(os.Stderr, "Please provide an absolute path")
+		os.Exit(1)
+	}
+
+	pj, ok := pjs.FindByRoot(root)
+	if ok {
+		todos := pj.TODOs()
+		fmt.Println(cli.TabTodos(todos))
 	}
 	os.Exit(0)
 }
